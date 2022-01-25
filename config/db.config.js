f83c674569a90f36391c6cd6906a9b7c7383f707
septianppm <sag@ppm-manajemen.ac.id>
2022-01-26 02:29:11 +0700

    clean update

--- config/db.config.js
+++ config/db.config.js
@@ -23,22 +23,13 @@ var db_config = {
 var dbConn;
 
 function handleDisconnect() {
-  dbConn = mysql.createPool(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  // dbConn.connect(function(err) {              // The server is either down
-  //   if(err) {                                     // or restarting (takes a while sometimes).
-  //     console.log('error when connecting to db:', err);
-  //     setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-  //   }                                     // to avoid a hot loop, and to allow our node script to
-  // });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
+  dbConn = mysql.createPool(db_config); 
   dbConn.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+      handleDisconnect();                         
+    } else {                                      
+      throw err;                                  
     }
   });
 }
--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const messagesController =   require('../controllers/messages.controller');
-// Retrieve all employees
+// Retrieve all messages
 router.get('/', messagesController.findAll);
-// Create a new employee
+// Create a new messages
 router.post('/', messagesController.create);
-// Retrieve a single employee with id
+// Retrieve a single messages with id
 router.get('/:id', messagesController.findById);
-// Update a employee with id
+// Update a messages with id
 router.put('/:id', messagesController.update);
-// Delete a employee with id
+// Delete a messages with id
 router.delete('/:id', messagesController.delete);
 module.exports = router
